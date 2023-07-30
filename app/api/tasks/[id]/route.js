54d097572eca21bffee701de882db416532e27e6
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-07-30 19:38:40 +0330

    getTask from database

--- app/api/tasks/[id]/route.js
+++ app/api/tasks/[id]/route.js
@@ -9,3 +9,10 @@ export async function PUT(request, { params }) {
   await Task.findByIdAndUpdate(id, { title, description });
   return NextResponse.json({ message: "task updated" }, { status: 200 });
 }
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const task = await Task.findOne({ _id: id });
+  return NextResponse.json({ task }, { status: 200 });
+}
--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -3,20 +3,42 @@ import React from "react";
 import { HiPencilAlt } from "react-icons/hi";
 import RemoveBtn from "./RemoveBtn";
 
-function TaskList() {
+const getTasks = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/tasks", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("faild to fetch tasks");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("error list: ", error);
+  }
+};
+
+async function TaskList() {
+  const { tasks } = await getTasks();
   return (
-    <div className=" border-2 border-solid border-black p-8 mb-4 flex justify-between">
-      <div>
-        <h1>topic title</h1>
-        <div>topic description</div>
-      </div>
-      <div className="flex justify-between items-center">
-        <RemoveBtn />
-        <Link href={"/editTask/1"}>
-          <HiPencilAlt size={24} />
-        </Link>
-      </div>
-    </div>
+    <>
+      {tasks.map((task) => (
+        <div
+          key={task.id}
+          className=" border-2 border-solid border-black p-8 mb-4 flex justify-between"
+        >
+          <div>
+            <h1 className=" font-extrabold text-2xl">{task.title}</h1>
+            <div className=" font-semibold">{task.description}</div>
+          </div>
+          <div className="flex justify-between items-center">
+            <RemoveBtn />
+            <Link href={`/editTask/${task._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
