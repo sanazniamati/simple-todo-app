9d43de5fd919b889e2f66d17708a76f70561ce37
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-07-31 09:17:42 +0330

    addtask form complete

--- app/addTask/page.jsx
+++ app/addTask/page.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 function AddTask() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("title and description is required");
+      return;
+    }
+    try {
+      const res = await fetch("http://localhost:3000/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "aplication/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("Faild to create a task");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form className="flex flex-col gap-4">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
       <input
         className=" w-full border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Task Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
+
       <input
         className=" w-full border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Task description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
-      <button className=" bg-green-600 font-bold text-white py-3 px-6 w-fit ">
+
+      <button
+        type="submit"
+        className=" bg-green-600 font-bold text-white py-3 px-6 w-fit "
+      >
         Add Task
       </button>
     </form>
